6af1640353e09cf2e8ed1c7e37878b6dff9ef3fb
castynet <riungemaina@gmail.com>
2022-01-06 22:17:00 +0300

    renamed

--- src/components/auth/Auth.spec.js
+++ src/components/auth/Auth.spec.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import App from "./App";
+import Auth from "./Auth";
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(<Auth />);
   expect(screen.getByTestId("wrapper")).toBeInTheDocument();
 });
