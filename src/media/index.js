2872a544399d85b51283ed86a605415bc9818951
castynet <riungemaina@gmail.com>
2022-01-12 20:44:34 +0300

    created

---
+++ src/media/index.js
@@ -0,0 +1,7 @@
+import controller from "./controller.svg";
+import finder from "./finder.svg";
+import intellect from "./intellect.svg";
+import messenger from "./messenger.svg";
+import whisperer from "./whisperer.svg";
+
+export const Media = { controller, finder, intellect, messenger, whisperer };
