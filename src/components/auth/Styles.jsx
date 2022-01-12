417a9cae27575bd23f55fc746b9e3d1b3681b73e
castynet <riungemaina@gmail.com>
2022-01-12 20:43:57 +0300

    components/auth/Styles: styled landing site

--- src/components/auth/Styles.jsx
+++ src/components/auth/Styles.jsx
@@ -1,12 +1,113 @@
 import styled from "styled-components";
+import { Media } from "../../media";
 
 export const Wrapper = styled.div`
-  width: 400px;
-  height: 500px;
-  background-color: var(--white);
-  box-shadow: var(--shadowTwo);
-  border-radius: var(--radius);
-  padding: 10px 20px;
-  margin: 0 auto 50px;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
+export const Content = styled.div`
+  width: 800px;
+  max-width: 95%;
+  height: 500px;
+  backdrop-filter: blur(3px) saturate(200%);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: var(--radius);
+  border: 1px solid var(--blackTransparent);
+  position: relative;
+  z-index: 99;
+  box-shadow: var(--shadowThree);
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+export const Info = styled.div`
+  width: 50%;
+`;
+
+export const Form = styled.div`
+  height: 100%;
+  width: 50%;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  h3 {
+    margin: 20px 0;
+  }
+
+  .formPara {
+    margin: 20px 0;
+
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      height: 12px;
+      border-bottom: solid 1px var(--blackTransparent);
+      width: 60px;
+    }
+
+    &::before {
+      margin: 0 0 0 -70px;
+    }
+
+    &::after {
+      margin: 0 0 0 10px;
+    }
+  }
+`;
+
+export const Image = styled.div`
+  &:before {
+    content: "";
+    display: inline-block;
+    position: absolute;
+    top: 50%;
+    -ms-transform: translateY(-50%);
+    transform: translateY(-50%);
+    margin-left: -100px;
+    width: 200px;
+    height: 200px;
+    background-image: url(${Media.controller});
+    background-size: contain;
+  }
+`;
+
+export const Intellect = styled.img`
+  width: 100%;
+  margin-bottom: -8px;
+`;
+
+export const LoginBtn = styled.button`
+  display: block;
+  margin: 10px 0;
+  padding: 15px 25px;
+  width: 100%;
+  background: var(--blue);
+  background: var(--gradientBlue);
+  font-family: var(--font);
+  color: var(--white);
+  border-radius: 1000px;
+  box-shadow: var(--shadow);
+  transition: var(--transition);
+
+  &:last-of-type {
+    background: var(--black);
+
+    &:hover {
+      background: var(--blackTwo);
+    }
+  }
+
+  &:hover {
+    background: var(--blueTwo);
+    box-shadow: var(--shadowTwo);
+  }
+`;
