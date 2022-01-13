84f2487abf91d3e6e302f8480033c8aa85715d77
castynet <riungemaina@gmail.com>
2022-01-13 18:03:02 +0300

    adapters/index: created & exported auth

---
+++ src/adapters/index.js
@@ -0,0 +1,5 @@
+import { signInWithPopup, signOut, cUsrWithEAndP, sInWithEAndP } from "./auth/auth";
+
+const Auth = { signInWithPopup, signOut, cUsrWithEAndP, sInWithEAndP };
+
+export default Auth;
