f577a6625b5f24005542ef1f8e1c4cc4943ed0e2
castynet <riungemaina@gmail.com>
2022-01-13 18:17:49 +0300

    Context: removed unused val warnings

--- src/Context.jsx
+++ src/Context.jsx
@@ -10,11 +10,13 @@ export const StateProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  const unUsedVal = { setIsLoading, setUser };
   return (
     <Context.Provider
       value={{
         user,
         isLoading,
+        unUsedVal,
       }}
     >
       {children}
