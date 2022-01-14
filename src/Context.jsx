466efe2cd1d2b4dad5bdfa6e2cb79212d35fa731
castynet <riungemaina@gmail.com>
2022-01-14 17:59:45 +0300

    Context: handled login

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,22 +1,63 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { auth, usr } from "adapters";
 
 const Context = React.createContext();
-
-export function useApp() {
-  return useContext(Context);
-}
+export const useApp = () => useContext(Context);
 
 export const StateProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const unUsedVal = { setIsLoading, setUser };
+  useEffect(() => {
+    (async function () {
+      try {
+        const user = await usr.getUser();
+        setUser(user);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  const registerEmail = (email, pass) => {
+    try {
+      auth.cUsrWithEAndP(email, pass);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const signInEmail = (email, pass) => {
+    try {
+      auth.sInWithEAndP(email, pass);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const signInGoogle = () => {
+    auth.signInWithRedirect();
+  };
+
+  const signOut = () => {
+    try {
+      auth.signOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Context.Provider
       value={{
         user,
+        setUser,
+        setIsLoading,
         isLoading,
-        unUsedVal,
+        registerEmail,
+        signInEmail,
+        signInGoogle,
+        signOut,
       }}
     >
       {children}
