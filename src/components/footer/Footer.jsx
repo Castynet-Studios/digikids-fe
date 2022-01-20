e2d972e975f12479a2f6233ad6a2f88dd0d80a34
castynet <riungemaina@gmail.com>
2022-01-20 09:59:22 +0300

    created

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import * as styled from "./Footer.Styles";
+
+export default function Footer() {
+  return (
+    <>
+      <styled.Wrapper>
+        <p>footer</p>
+      </styled.Wrapper>
+    </>
+  );
+}
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,11 @@
+import * as styled from "./Nav.Styles";
+
+export default function Nav() {
+  return (
+    <>
+      <styled.Wrapper>
+        <p>Nav</p>
+      </styled.Wrapper>
+    </>
+  );
+}
