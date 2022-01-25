2539b4dde82265ac933fcfb3ccd3264cb177c240
castynet <riungemaina@gmail.com>
2022-01-25 11:05:10 +0300

    elements/icons/icons: added loader

--- src/elements/icons/icons.js
+++ src/elements/icons/icons.js
@@ -1,9 +1,11 @@
 import { UserPlus } from "@styled-icons/boxicons-solid";
 import { UserAccount } from "@styled-icons/boxicons-solid";
 import { Google } from "@styled-icons/boxicons-logos";
+import { LoaderAlt } from "@styled-icons/boxicons-regular/LoaderAlt";
 
 export const Icons = {
   RegisterUser: UserPlus,
   Account: UserAccount,
   Google: Google,
+  Loader: LoaderAlt,
 };
