b5c1e103ae58788b14edc47eadd5034d144eeb99
castynet <riungemaina@gmail.com>
2022-01-25 11:03:36 +0300

    components/auth/login/Login: added login form

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import * as styled from "./Styles.Login";
 
 export default function Login() {
+  const [showPass, setShowPass] = useState(false);
+
   return (
-    <>
-      <styled.Wrapper>Login</styled.Wrapper>
-    </>
+    <styled.Wrapper>
+      <h3>Login</h3>
+      <form>
+        <styled.Label htmlFor="email">Email</styled.Label>
+        <styled.Input
+          placeholder="omondi@gmail.com"
+          type="email"
+          name="Email"
+          id="email"
+        />
+        <styled.CheckBoxWrap>
+          <input
+            type="checkbox"
+            onClick={() => setShowPass(!showPass)}
+            id="checkPass"
+          />
+          <label htmlFor="checkPass">Remember me</label>
+        </styled.CheckBoxWrap>
+        <styled.Submit type="submit" value="Login" />
+      </form>
+      <p className="formPara">Don't have an Account yet?</p>
+      <styled.AuthBtn to="/auth">Register</styled.AuthBtn>
+    </styled.Wrapper>
   );
 }
