f3a67009e20d63d9f96854233de259f5aa43fff1
castynet <riungemaina@gmail.com>
2022-01-25 11:05:34 +0300

    components/auth/register/Register: added link home

--- src/components/auth/register/Register.jsx
+++ src/components/auth/register/Register.jsx
@@ -2,36 +2,34 @@ import * as styled from "./Styles.Register";
 
 export default function Register() {
   return (
-    <>
-      <styled.Wrapper>
-        <h3>Register</h3>
-        <form>
-          <styled.Label htmlFor="firstName">First Name</styled.Label>
-          <styled.Input
-            placeholder="John"
-            type="text"
-            name="First Name"
-            id="firstName"
-          />
-          <styled.Label htmlFor="lastName">Last Name</styled.Label>
-          <styled.Input
-            placeholder="Omondi"
-            type="text"
-            name="Last Name"
-            id="lastName"
-          />
-          <styled.Label htmlFor="email">Email</styled.Label>
-          <styled.Input
-            placeholder="omondi@gmail.com"
-            type="email"
-            name="Email"
-            id="email"
-          />
-          <styled.Submit type="submit" value="Register" />
-        </form>
-        <p className="formPara">Already have an Account?</p>
-        <styled.AuthBtn>Register</styled.AuthBtn>
-      </styled.Wrapper>
-    </>
+    <styled.Wrapper>
+      <h3>Register</h3>
+      <form>
+        <styled.Label htmlFor="firstName">First Name</styled.Label>
+        <styled.Input
+          placeholder="John"
+          type="text"
+          name="First Name"
+          id="firstName"
+        />
+        <styled.Label htmlFor="lastName">Last Name</styled.Label>
+        <styled.Input
+          placeholder="Omondi"
+          type="text"
+          name="Last Name"
+          id="lastName"
+        />
+        <styled.Label htmlFor="email">Email</styled.Label>
+        <styled.Input
+          placeholder="omondi@gmail.com"
+          type="email"
+          name="Email"
+          id="email"
+        />
+        <styled.Submit type="submit" value="Register" />
+      </form>
+      <p className="formPara">Already have an Account?</p>
+      <styled.AuthBtn to="/auth">Login</styled.AuthBtn>
+    </styled.Wrapper>
   );
 }
